2e3d24d575db09eb031d3b9cdaf1d620594e5017
kareeems <kareemsyukur@gmail.com>
2024-07-16 17:48:21 +0700

    nganu

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ListView Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Map<String, String>> items = List.generate(
+   50,
+        (index) => {
+      'title': 'Item $index',
+      'subtitle': 'Subtitle $index',
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Barang'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.list),
+            title: Text(items[index]['title']!),
+            subtitle: Text(items[index]['subtitle']!),
+          );
+        },
+      ),
+    );
+  }
+}
